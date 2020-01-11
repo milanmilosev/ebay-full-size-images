cc07cc7ec70243ecfc49c9940466d94c30f50dbc
milanmilosev <mmiloshev@gmail.com>
2020-01-11 13:08:39 +0100

    code improvement-fixed issue with new gallery

--- content.js
+++ content.js
@@ -1,21 +1,34 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.message === "FETCH_IMAGE") {
-    // Extract images from product page or gallery
-    const imageContainer =
-      document.querySelectorAll(".ad-image-wrapper .ad-image img") ||
-      document.querySelectorAll("imagebox-thumbnail img");
-    // Get all image sources
-    const imageArray = Array.from(imageContainer);
-    const imageSource =
-      imageArray.length && imageArray.map(src => src.getAttribute("src"));
+    // open gallery 
+    document.querySelector("#viewad-image") && 
+    document.querySelector("#viewad-image").click()
+    
+    // settimeout to prevent side effects
+    setTimeout(() => {
+      // extract images
+      const imageContainer =
+        document.querySelectorAll(".ad-image-wrapper .ad-image img") ||
+        document.querySelectorAll("imagebox-thumbnail img") || false;
 
-    console.log(imageSource[0]);
-    // Listener
-    chrome.runtime.sendMessage({
-      message: "SHOW_RESULTS",
-      results: {
-        imageSource: imageSource
-      }
-    });
+    
+      // create images array
+      const imageArray = Array.from(imageContainer) || null;
+      const imageSource = 
+        imageArray.length && 
+        imageArray.map(src => src.getAttribute("src"));
+
+      // send results to background listener
+      chrome.runtime.sendMessage({
+        message: "SHOW_RESULTS",
+        results: {
+          imageSource: imageSource
+        }
+      });
+
+      // close gallery
+      imageSource ? document.querySelector(".mfp-close").click() : null
+
+    }, 1000)
   }
 });
